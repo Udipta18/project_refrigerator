f2615a5b45c85507daea1d87a7d67070dcdb8b2a
Udipta <dasdsa1208@gmail.com>
2024-03-18 10:20:06 +0530

    body changes

---
+++ production-client/src/app/card-body/card-body.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-body',
+  standalone: true,
+  templateUrl: './card-body.component.html',
+  styleUrl: './card-body.component.css'
+})
+export class CardBodyComponent {
+
+}
