ce57e18914d652435dde7db67fb889fa4ad2ebd9
Udipta <dasdsa1208@gmail.com>
2024-03-19 22:07:17 +0530

    whatsapp feature add

--- production-client/src/app/app.component.ts
+++ production-client/src/app/app.component.ts
@@ -4,11 +4,12 @@ import { CardBodyComponent } from './card-body/card-body.component';
 import { ReviewComponent } from './review/review.component';
 import { FixedSideNavComponent } from './fixed-side-nav/fixed-side-nav.component';
 import { FooterComponent } from './footer/footer.component';
+import { NewsTickerComponent } from './news-ticker/news-ticker.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,CardBodyComponent,ReviewComponent,FixedSideNavComponent,FooterComponent],
+  imports: [RouterOutlet,CardBodyComponent,ReviewComponent,FixedSideNavComponent,FooterComponent,NewsTickerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   schemas:[CUSTOM_ELEMENTS_SCHEMA]
